35970694485a1d86bb19b7ab322b2f2c43ae4a3f
samuelmarcos <samuelmarcos9580@gmail.com>
2023-11-30 21:14:23 -0300

    feat: add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,14 @@
+import { type AuthenticationError } from '../errors'
+import { type AccessToken } from '../models'
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
