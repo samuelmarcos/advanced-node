17f9ca6bda497c441dac5ce9f15899ff3863cce0
samuelmarcos <samuelmarcos9580@gmail.com>
2023-12-01 17:48:36 -0300

    refactor: chanege sut strategy to use beforeEach

--- tests/data/services/facebook-athentication.spec.ts
+++ tests/data/services/facebook-athentication.spec.ts
@@ -2,32 +2,25 @@ import { AuthenticationError } from '@/domain/errors'
 import { type LoadFacebookUserApi } from '@/data/contracts/apis/facebook'
 import { FacebookAthenticationService } from '@/data/services'
 import { mock, type MockProxy } from 'jest-mock-extended'
-import { type FacebookAuthentication } from '@/domain/features'
-
-type SutTypes = {
-  sut: FacebookAuthentication
-  loadFacebookUserSpy: MockProxy<LoadFacebookUserApi>
-}
-
-const makeSut = (): SutTypes => {
-  const loadFacebookUserSpy = mock<LoadFacebookUserApi>()
-  const sut = new FacebookAthenticationService(loadFacebookUserSpy)
-
-  return { sut, loadFacebookUserSpy }
-}
 
 describe('Facebook Athentication Service', () => {
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let sut: FacebookAthenticationService
+
+  beforeEach(() => {
+    loadFacebookUserApi = mock()
+    sut = new FacebookAthenticationService(loadFacebookUserApi)
+  })
+
   it('shoud call LoadFacebookUserApi with correct params', async () => {
-    const { sut, loadFacebookUserSpy } = makeSut()
     await sut.perform({ token: 'any_token' })
-    expect(loadFacebookUserSpy.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
-    expect(loadFacebookUserSpy.loadUser).toHaveBeenCalledTimes(1)
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('shoud return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const { sut, loadFacebookUserSpy } = makeSut()
     const authResult = await sut.perform({ token: 'any_token' })
-    loadFacebookUserSpy.loadUser.mockResolvedValueOnce(undefined)
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
     expect(authResult).toEqual(new AuthenticationError())
   })
 })
