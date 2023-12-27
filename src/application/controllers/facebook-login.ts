b1b94308a79b5dd04659e282242adbce6a259c5a
samuelmarcos <samuelmarcos9580@gmail.com>
2023-12-27 09:58:34 -0300

    extract validate to a method

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -16,10 +16,17 @@ export class FacebookLoginController {
 
   public async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
+      const error = this.validate(httpRequest)
+      if (error !== undefined) {
+        return badRequest(error)
+      }
+
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError('token'))
       }
+
       const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+
       if (accessToken instanceof AccessToken) {
         return ok({ accessToken: accessToken.value })
       } else {
@@ -27,6 +34,12 @@ export class FacebookLoginController {
       }
     } catch (error) {
       return serverError(error)
+    }
+  }
+
+  private validate (httpRequest: HttpRequest): Error | undefined {
+    if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
+      return new RequiredFieldError('token')
     }
   }
 }
