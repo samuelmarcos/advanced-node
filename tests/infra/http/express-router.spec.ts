8a630dbfe13469c02b51ef41be7475bde0d056d1
samuelmarcos <samuelmarcos9580@gmail.com>
2024-01-20 23:08:53 -0300

    ensure express router respond with 400 if valid error

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -7,7 +7,11 @@ class ExpressRouter {
   constructor (private readonly controller: Controller) {}
   public async adapt (req: Request, res: Response): Promise<void> {
     const httpResponse = await this.controller.handle({ ...req.body })
-    res.status(httpResponse.statusCode).json(httpResponse.data)
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.data)
+    } else {
+      res.status(400).json({ error: httpResponse.data.message })
+    }
   }
 }
 
@@ -48,6 +52,20 @@ describe('Express Router', () => {
     expect(res.status).toHaveBeenCalledWith(200)
     expect(res.status).toHaveBeenCalledTimes(1)
     expect(res.json).toHaveBeenCalledWith({ data: 'any_data' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
+
+  it('should respond with 400 and valid error', async () => {
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 400,
+      data: new Error('any_error')
+    })
+
+    await sut.adapt(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
