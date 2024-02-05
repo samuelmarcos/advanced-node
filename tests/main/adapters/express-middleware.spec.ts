904d5ec0b9c73d68340905a390acd26802fa0cfc
samuelmarcos <samuelmarcos9580@gmail.com>
2024-02-05 20:36:18 -0300

    ensure ExpressMiddleware calls handle with correct request

---
+++ tests/main/adapters/express-middleware.spec.ts
@@ -0,0 +1,45 @@
+import { type RequestHandler, type Request, type Response, type NextFunction } from 'express'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { type HttpResponse } from '@/application/helpers'
+import { type MockProxy, mock } from 'jest-mock-extended'
+
+interface Middleware {
+  handle: (httpRequest: any) => Promise<HttpResponse>
+}
+
+type Adapter = (middleware: Middleware) => RequestHandler
+
+const adaptExpressMiddleware: Adapter = (middleware) => async (req, res, next) => {
+  await middleware.handle({ ...req.headers })
+}
+
+describe('ExpressMiddleware', () => {
+  let req: Request
+  let res: Response
+  let next: NextFunction
+  let sut: RequestHandler
+  let middleware: MockProxy<Middleware>
+
+  beforeEach(() => {
+    req = getMockReq({ headers: { any: 'any' } })
+    res = getMockRes().res
+    next = getMockRes().next
+    middleware = mock()
+    sut = adaptExpressMiddleware(middleware)
+  })
+
+  it('shoud call handle with correct request', async () => {
+    await sut(req, res, next)
+
+    expect(middleware.handle).toHaveBeenCalledWith({ ...req.headers })
+    expect(middleware.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('shoud call handle with empty request', async () => {
+    req = getMockReq({ headers: {} })
+    await sut(req, res, next)
+
+    expect(middleware.handle).toHaveBeenCalledWith({ })
+    expect(middleware.handle).toHaveBeenCalledTimes(1)
+  })
+})
