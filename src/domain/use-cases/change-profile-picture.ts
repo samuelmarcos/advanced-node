1995d1ceef10f4a98b4019e7585ff986a883b67d
samuelmarcos <samuelmarcos9580@gmail.com>
2024-02-19 21:29:12 -0300

    ensure ChangeProfilePicture not call filestorage when file is undefined

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -1,9 +1,11 @@
 import { type UploadFile, type UUIDGenerator } from '@/domain/contracts/gateways'
 
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
-type Input = { id: string, file: Buffer }
+type Input = { id: string, file?: Buffer }
 export type ChangeProfilePicture = (input: Input) => Promise<void>
 
 export const setupChangeProfilePicure: Setup = (fileStorage, crypto) => async ({ id, file }) => {
-  await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
+  if (file !== undefined) {
+    await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
+  }
 }
