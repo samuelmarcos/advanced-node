92439b658a6648f350ce9d3f771b272bcc68093e
samuelmarcos <samuelmarcos9580@gmail.com>
2024-03-04 20:15:22 -0300

    ensure FacebookLoginController to instance of Controller

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,4 +1,4 @@
-import { FacebookLoginController } from '@/application/controllers'
+import { Controller, FacebookLoginController } from '@/application/controllers'
 import { UnauthorizedError } from '@/application/errors'
 import { RequiredStringValidator } from '@/application/validation'
 
@@ -16,6 +16,10 @@ describe('FacebookLoginController', () => {
 
   beforeEach(() => {
     sut = new FacebookLoginController(facebookAuth)
+  })
+
+  it('shoud extend Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
   })
 
   it('shoud build Validators correctly', async () => {
