6dff5201a4b0a84b4f68c0375cd67a82321cbc80
samuelmarcos <samuelmarcos9580@gmail.com>
2024-03-04 20:02:49 -0300

    fix PgUserProfileRepository loads user profile

--- src/infra/repos/postgres/user-profile.ts
+++ src/infra/repos/postgres/user-profile.ts
@@ -1,11 +1,18 @@
 import { getRepository } from 'typeorm'
 import { PgUser } from './entities'
-import { type SaveUserPicture } from '@/domain/contracts/repos'
+import { type LoadUserProfile, type SaveUserPicture } from '@/domain/contracts/repos'
 
 export class PgUserProfileRepository implements SaveUserPicture {
   public async savePicture ({ id, pictureUrl, initials }: SaveUserPicture.Input): Promise<void> {
     const pgUserRepo = getRepository(PgUser)
 
     await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
+  }
+
+  public async load ({ id }: LoadUserProfile.Input): Promise<LoadUserProfile.Output> {
+    const pgUserRepo = getRepository(PgUser)
+
+    const pgUser = await pgUserRepo.findOne({ id: parseInt(id) })
+    if (pgUser !== undefined) return { name: pgUser.name }
   }
 }
