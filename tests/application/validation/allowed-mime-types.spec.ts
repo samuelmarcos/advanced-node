adc3b78937d0beed763fc96fe2f0d0f32a68e389
samuelmarcos <samuelmarcos9580@gmail.com>
2024-05-06 20:41:17 -0300

    move code from test to source

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -1,26 +1,5 @@
+import { AllowedMimeTypes } from '@/application/validation'
 import { InvalidMimeTypeError } from '@/application/errors'
-
-type Entension = 'png' | 'jpg'
-
-class AllowedMimeTypes {
-  constructor (private readonly allowed: Entension[],
-    private readonly mimetype: string) {}
-
-  validate (): Error | undefined {
-    let isValid = false
-    if (this.isPng()) isValid = true
-    else if (this.isJpg()) isValid = true
-    if (!isValid) return new InvalidMimeTypeError(this.allowed)
-  }
-
-  private isPng (): boolean {
-    return this.allowed.includes('png') && this.mimetype === 'image/png'
-  }
-
-  private isJpg (): boolean {
-    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimetype)
-  }
-}
 
 describe('AllowedMimeTypes', () => {
   it('should return InvalidMimeTypeError if value is invalid', () => {
