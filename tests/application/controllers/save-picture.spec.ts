ebb248474878b9e8cdfa35caf1813545a86a3091
samuelmarcos <samuelmarcos9580@gmail.com>
2024-05-07 19:44:23 -0300

    move code from tests to its own folder

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,37 +1,5 @@
-import { badRequest, ok, type HttpResponse } from '@/application/helpers/http'
-import { RequiredFieldError } from '@/application/errors'
-import { type ChangeProfilePicture } from '@/domain/use-cases'
-
-type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string }
-type Model = Error | { pictureUrl?: string, initials?: string }
-
-export class SavePictureController {
-  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
-
-  public async perform ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
-    if (file === undefined || file == null) return badRequest(new RequiredFieldError('file'))
-    if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
-    if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
-    if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
-
-    const data = await this.changeProfilePicture({ id: userId, file: file.buffer })
-    return ok(data)
-  }
-}
-
-export class InvalidMimeTypeError extends Error {
-  constructor (allowed: string[]) {
-    super(`Unsuported type. Allowed type: ${allowed.join(', ')}`)
-    this.name = 'InvalidMimeTypeError'
-  }
-}
-
-export class MaxFileSizeError extends Error {
-  constructor (maxSizeInMb: number) {
-    super(`File upload limit is ${maxSizeInMb}MB`)
-    this.name = 'MaxFileSizeError'
-  }
-}
+import { InvalidMimeTypeError, MaxFileSizeError, RequiredFieldError } from '@/application/errors'
+import { SavePictureController } from '@/application/controllers'
 
 describe('SavePictureController', () => {
   let buffer: Buffer
