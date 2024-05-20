0b5aa1c4fd566a550163dec2b0a1c632566c423e
samuelmarcos <samuelmarcos9580@gmail.com>
2024-05-20 20:57:19 -0300

    chore: add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,10 @@ export const env = {
     clientSecret: process.env.FB_CLIENT_SECRET ?? '9aacaf0a0cff805879c23dd0d3b91610'
   },
   jwtSecret: process.env.JWT_SECRET ?? 'ABACATE',
-  appPort: process.env.PORT ?? '8080'
+  appPort: process.env.PORT ?? '8080',
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY || '',
+    secret: process.env.AWS_S3_SECRET || '',
+    bucket: process.env.AWS_S3_BUCKET || ''
+  }
 }
---
+++ tests/external/aws-s3-file-storage.test.ts
@@ -0,0 +1,24 @@
+import { AwsS3FileSotrage } from '@/infra/gateways/aws-se-file-storage'
+import { env } from '@/main/config/env'
+
+import axios from 'axios'
+
+describe('AWS S3 Integration Test', () => {
+  let sut: AwsS3FileSotrage
+
+  beforeEach(() => {
+    sut = new AwsS3FileSotrage(env.s3.accessKey, env.s3.secret, env.s3.bucket)
+  })
+
+  it('should upload and delete an image to aws s3', async () => {
+    const pixelImage = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+L+U4T8ABu8CpCYJ1DQAAAAASUVORK5CYII='
+    const file = Buffer.from(pixelImage, 'base64')
+    const key = 'any_key.png'
+
+    const [pictureUrl] = await sut.upload({ key, file })
+
+    await sut.delete({ key })
+
+    await expect(axios.get(pictureUrl)).rejects.toThrow()
+  })
+})
