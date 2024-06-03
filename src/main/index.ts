d29aeb9fca074d35b204425a125045b7c50556b6
samuelmarcos <samuelmarcos9580@gmail.com>
2024-06-03 20:39:27 -0300

    refactor: move typeorm config to a ts file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,14 +1,11 @@
 import './config/module-alias'
 import 'reflect-metadata'
-import { createConnection, getConnectionOptions } from 'typeorm'
+import { createConnection } from 'typeorm'
 
 import { env } from './config/env'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-getConnectionOptions().then(async options => {
-  const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
-  const entities = [`${root}/infra/repos/postgres/entities/index.{js,ts}`]
-  await createConnection({ ...options, entities })
+createConnection().then(async () => {
   const { app } = await import ('./config/app')
   app.listen(env.appPort, () => console.log(`Server running on port ${env.appPort}`))
 }).catch(console.error)
