f2c859eaae22b28e3a13670b563ebe4d2a00de32
samuelmarcos <samuelmarcos9580@gmail.com>
2024-06-03 20:09:54 -0300

    fix: fix connection problems

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,14 @@
 import './config/module-alias'
+import 'reflect-metadata'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
 import { env } from './config/env'
-import { app } from './config/app'
 
-import 'reflect-metadata'
-import { createConnection } from 'typeorm'
-
-createConnection()
-  .then(() => {
-    app.listen(env.appPort, () => console.log(`Server running on port ${env.appPort}`))
-  })
-  .catch(console.error)
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+getConnectionOptions().then(async options => {
+  const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
+  const entities = [`${root}/infra/repos/postgres/entities/index.{js,ts}`]
+  await createConnection({ ...options, entities })
+  const { app } = await import ('./config/app')
+  app.listen(env.appPort, () => console.log(`Server running on port ${env.appPort}`))
+}).catch(console.error)
